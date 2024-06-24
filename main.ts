589df79799eea99fac25727bb76e5d0bb978bcdc
asmaiqbal85 <162962114+asmaiqbal85@users.noreply.github.com>
2024-06-23 22:36:24 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todos : string[] = [];
+let condition = true;
+
+
+while(condition){
+let addtask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message: "whant do you want to add in your todo?",
+        },
+        {
+           name: "addMore",
+           type: "confirm",
+           message: "Do you want to add more todo?",
+           default: "false" 
+        }
+    ]
+);
+todos.push(addtask.todo);
+condition = addtask.addMore
+console.log(todos)
+}
